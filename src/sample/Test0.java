8e58f39054237922e5e4b8c91dcb27f4901dff7a
QSP <QSP@DESKTOP-EVK9GMC>
2021-03-05 11:21:38 +0530

    Demo2

--- src/sample/Test0.java
+++ src/sample/Test0.java
@@ -4,6 +4,8 @@ public class Test0 {
 
 	public static void main(String[] args) {
 		System.out.println("hello java");
+		System.out.println("Hello python");
+		System.err.println("Hello appium");
 	}
 
 }
